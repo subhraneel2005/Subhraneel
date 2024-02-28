45d7819ac629f3b89335b1c002e5cdeff36cd361
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-28 21:03:51 +0530

    simple ui fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import bgVideo from '../assets/bgVideo.mp4';
-import bgAudio from '../assets/bgAudio.mp3';
+import bgImg from '../assets/bgImg.jpg'
 import { useNavigate } from 'react-router-dom';
 import Connect from './Connect';
 
@@ -13,21 +12,14 @@ function HomePage() {
     const contacts = '/contacts'
   return (
     <div className="relative h-screen select-none">
-      <video
-        className="absolute inset-0 w-full h-full object-cover brightness-50"
-        autoPlay
-        muted
-        loop
-      >
-        <source src={bgVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <img src={bgImg} className="absolute inset-0 w-full h-full object-cover brightness-50 bg-cover">
+      </img>
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className='block'>
-            <h1 className="text-[10vw] font-bold text-white glow">S U B H R A N E E L</h1>
-            <h1 className="text-[6vw] text-center font-bold text-white glowtwo">React Developer</h1>
+        <div className='block mt-24'>
+            <h1 className="text-[7vw] text-center font-bold text-white glow">S U B H R A N E E L</h1>
+            <h1 className="text-[4vw] text-center font-bold text-white glowtwo">React Developer</h1>
 
-            <div className='flex justify-evenly mt-10'>
+            <div className='flex justify-evenly mt-10 gap-4'>
                 <button className='black-glow px-4 py-2 rounded-xl text-[3vw] cursor-pointer text-blue-300'
                 onClick={() => navigate(skills)}
                 >Skills</button>
