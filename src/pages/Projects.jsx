f7771642c9e45b70fd7394044b7b9f660b1ccebf
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-03-01 04:58:25 +0530

    new project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import b from '../assets/b.jpg';
 import d from '../assets/d.jpg';
+import two from '../assets/two.jpg'
 
 function Projects() {
   return (
     <div className='h-screen w-full flex justify-center items-center bg-sky-500 select-none'>
         <div className='h-full w-full flex justify-center items-center gap-5 flex-wrap'>
+
+            <div className='w-[350px] h-[400px] rounded-xl flex justify-center items-center text-sky-200 bg-sky-800'>
+                <div className='block p-4'>
+                    <h1 className='text-4xl font-bold text-center text-sky-100 mb-[50px]'>Ecommerce Website</h1>
+                    <img src={d} alt=""  className='w-[80%] h-[50%] ml-8'/>
+                    <ul>
+                    <li className='text-3xl font-bold text-center cursor-pointer hover:text-white duration-300 decoration-white'><a href="https://subhraneel-ecommerce.vercel.app/" target='blank'>Live Demo</a></li>
+                    <li className='text-3xl font-bold text-center cursor-pointer hover:text-white duration-300 decoration-white'><a href="https://github.com/subhraneel2005/Ecommerce" target='blank'>GitHub repo</a></li>
+                        <li className='text-xl font-bold text-center'>Ecommerce website made using React Js.</li>
+                    </ul>
+                </div>
+            </div>
+
             <div className='w-[350px] h-[400px] rounded-xl flex justify-center items-center bg-sky-800 text-sky-200'>
                 <div className='block p-4'>
                     <h1 className='text-4xl font-bold text-center text-sky-100
